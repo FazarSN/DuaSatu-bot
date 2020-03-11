caaab17cbc89eecb5ccbb558182b784f8d7ad1bc
fazar <fazar.nugroho@cxrus.com>
2020-03-11 13:15:10 +0700

    initial commit

---
+++ bot.js
@@ -0,0 +1,30 @@
+// Run dotenv
+require('dotenv').config();
+
+// Import libraries
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// Event listener when a user connected to the server.
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Event listener when a user sends a message in the chat.
+client.on('message', message => {
+
+  if (message.author.bot) return;
+  let command = message.content.split(" ")[0];
+  
+  let args = message.content.split(" ").slice(1);
+  
+  if (command == "!delete") {
+	message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send("Deleted "+args[0]+" messages.").then(msg => msg.delete(5000));  
+  }).catch(console.error);
+  }
+
+});
+
+// Initialize bot by connecting to the server
+client.login(process.env.DISCORD_TOKEN);
