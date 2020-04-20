6b96857429aa1cc7096ed3c2152927d00175a89f
fazar <fazar.nugroho@cxrus.com>
2020-04-20 18:09:41 +0700

    add clear all

--- bot.js
+++ bot.js
@@ -11,6 +11,17 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+async function f(message) {
+	var currentChannel = message.channel;
+	var f = null;
+	for (var i = 0; i < 1000; i++) {
+		f = currentChannel.fetchMessages({ limit: 1 }).then(msg => {					
+			msg.first().delete().catch(console.log("process1"));
+		}).catch(console.log("process2"));
+	}
+	await f;
+}
+
 // Event listener when a user sends a message in the chat.
 client.on('message', message => {
 	if (message.author.bot) return;
@@ -23,10 +34,16 @@ client.on('message', message => {
 			message.channel.bulkDelete(args[0]).then(() => { message.channel.send("Deleted "+args[0]+" messages.").then(msg => msg.delete(1000));
 			}).catch(console.error);
 		} else if (command == "!welcome") {
-      message.channel.send("Selamat Datang!");
-    }
+			message.channel.send("Selamat Datang!");
+		} else if (command == "!clearall") {
+			f(message);
+			message.channel.send("Clear Done!");
+			console.log("clear done");
+		} else if (command == "!testbot") {
+			console.log("Tested!");
+		}
 	} else {
-		console.log(message.author.username)
+		console.log(message.author.id + " - " + message.author.username)
 		return;
 	}
 });
