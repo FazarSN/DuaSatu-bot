bcd068b509b9b8506d86babf6059e77e523614b0
fazar <fazar.nugroho@cxrus.com>
2020-11-23 11:39:16 +0700

    add image variable

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DuaSatu-bot
+
+1. delete template_ in template.env and fill out the token
+2. run node bot.js
--- bot.js
+++ bot.js
@@ -44,10 +44,11 @@ client.on('message', message => {
 		} else if (command == "!sendlink") {
 			message.channel.bulkDelete(1);
 			console.log("sending message");
-			var judul = "Episode 6 - F.R.I.E.N.D.S"
-			var listennotes = "https://lnns.co/ciXEQK3bafI";
-			var spotify = "https://open.spotify.com/episode/3YGGGTN0NBAhbEEdddmjN5?si=qxRhLmhYTYyXBxpAdMqbbw";
-			message.channel.send(judul + " \n\n listennotes : " + listennotes + "\n spotify : " + spotify, {files: ["episode-6.jpg"]});
+			var judul = "Episode 8 - The Fault in Our Stars"
+			var listennotes = "https://lnns.co/8aOMXw14NDK";
+			var spotify = "https://open.spotify.com/episode/41lcFsNU4qJybl0WLyszqq?si=ziS1lPsMSSeUrNl4tnnrVg";
+			var imageFile = "episode-8.jpg";
+			message.channel.send(judul + " \n\n listennotes : " + listennotes + "\n spotify : " + spotify, {files: [imageFile]});
 		}
 	} else {
 		console.log(message.author.id + " - " + message.author.username)
